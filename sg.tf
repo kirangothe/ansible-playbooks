e0a90f4e998c8e6afa2f4053caffad60992307e6
Kiran Gothe <keyrun.gothe@gmail.com>
2023-04-28 21:42:51 +0545

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,6 +1,14 @@
 //Security group creation and whitelisting the ip
 resource "aws_security_group" "allow_tls" {
   name = "terraform-sg"
+  
+  ingress {
+    description = "Allow port 80 - inbound"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "Allow port 22 - inbound"
