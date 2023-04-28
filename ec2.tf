e86f05ba69f852b3e9ebcedeb2fd27b593117c9c
Kiran Gothe <keyrun.gothe@gmail.com>
2023-04-28 22:11:42 +0545

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myec2" {
   ami                    = "ami-08df646e18b182346"
   instance_type          = "t2.micro"
-  availability_zone = "ap-southeast-2"
+  availability_zone = "ap-southeast-2a"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name = "kgsd"
 
