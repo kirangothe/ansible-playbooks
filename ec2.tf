1c1982d25a4e9775a177882c947dddcf1f82ca07
Kiran Gothe <keyrun.gothe@gmail.com>
2023-04-28 21:47:03 +0545

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "myec2" {
   ami                    = "ami-08df646e18b182346"
   instance_type          = "t2.micro"
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap-southeast-2"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name = "pswain"
 
   tags = {
-    name = "testec2"
+    name = "php-mysql"
   }
 
   provisioner "local-exec" {
